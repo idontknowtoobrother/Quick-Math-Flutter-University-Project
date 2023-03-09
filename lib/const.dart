5c2fa9422cb5dc63347f354eb9ccbdd9bc2012c2
Akapon Chayangkanont <62160317@go.buu.ac.th>
2023-03-10 03:25:37 +0700

    const.dart

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const colorGreen = Color(0xFF91C778);
+const colorWhite = Color(0xFFFFFFFF);
+const colorBackground = Color(0xFFF3F4F6);
+const colorGrey = Color(0xFF414141);
+const colorRed = Color(0xFFFF7081);
