dcb3b20bc00e9560b8763e004e9e36b6231696a6
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-03-10 11:05:18 +0700

    done: lose page

--- lib/lose_page.dart
+++ lib/lose_page.dart
@@ -16,33 +16,58 @@ class _LosePageState extends State<LosePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.red,
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            Expanded(
-              flex: 0,
-              child: Container(
-                child: ActionButton(
-                  info: 'RESTART',
-                  bgColor: colorRed,
-                  callBack: () => {
-                    Navigator.of(context).pop(),
-                  },
+              Expanded(
+                flex: 1,
+                child: Container(),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.text,
+                        style: TextStyle(
+                          fontSize: 70,
+                          fontWeight: FontWeight.bold,
+                          color: colorRed,
+                        ),
+                      ),
+                      Text(
+                        'SCORE : ${widget.score}',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: colorGrey,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            )
-          ],
+              Expanded(
+                flex: 0,
+                child: Container(
+                  child: ActionButton(
+                    info: 'RESTART',
+                    bgColor: colorRed,
+                    callBack: () => {
+                      Navigator.of(context).pop(),
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
+          ),
         ),
       ),
     );
